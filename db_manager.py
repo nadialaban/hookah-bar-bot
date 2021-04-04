90c4a7004bdcad43bcd9e4c86b23bc5682098670
Nadia Laban <naabulaban@gmail.com>
2021-04-04 23:40:24 +0300

    first commit

--- db_manager.py
+++ db_manager.py
@@ -1,34 +1,40 @@
 import sqlalchemy
 from sqlalchemy import create_engine
+from sqlalchemy import Column
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 from config import *
 
 # 1. Подключение к бд.
 db_string = "postgres://{}:{}@{}:{}/{}".format(DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
 db = create_engine(db_string)
-
+base = declarative_base()
+Session = sessionmaker(db)
+session = Session()
 
 # 2. Описание моделей
 # 2.1. Сообщение от бота
-class BotMessage(db.Model):
+class BotMessage(base):
     __tablename__ = 'messages'
 
-    id = db.Column(db.Text, primary_key=True)
-    message = db.Column(db.Text)
-    description = db.Column(db.Text)
+    id = Column(sqlalchemy.Text, primary_key=True)
+    message = Column(sqlalchemy.Text)
+    description = Column(sqlalchemy.Text)
 
 
 # 2.1. Пользователь
 class User(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Text)
-    status = db.Column(db.Text)
-    phone = db.Column(db.Text)
-    birthday = db.Column(db.Date)
-    confirmed = db.Column(db.Boolean)
-    confirm_msg = db.Column(db.Integer)
-    action = db.Column(db.Text)
+    id = Column(sqlalchemy.Integer, primary_key=True)
+    name = Column(sqlalchemy.Text)
+    status = Column(sqlalchemy.Text)
+    phone = Column(sqlalchemy.Text)
+    birthday = Column(sqlalchemy.Date)
+    confirmed = Column(sqlalchemy.Boolean)
+    confirm_msg = Column(sqlalchemy.Integer)
+    action = db.Column(sqlalchemy.Text)
 
 
 def get_message(msg_id):
